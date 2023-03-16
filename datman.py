989b509958f6f5148f4d5444855876696c36c466
V <82741743+SamuraiOndo@users.noreply.github.com>
2023-03-16 09:25:46 -0500

    Adds ARC/filename support.

--- datman.py
+++ datman.py
@@ -47,7 +47,8 @@ for dat_file in files:
                 for j in range(count):
                     pointer2 = reader.read_uint32()
                     size2 = reader.read_uint32()
-                    hash = reader.read_uint32()
+                    filenameOffset = reader.read_uint32()
+                    hash = False
                     filename = f"{i}-{j}"
                     if (size2 == 0):
                         filename = "BLANK"
@@ -74,16 +75,23 @@ for dat_file in files:
                                 raise Exception("magic length less than 3")
                         except:
                             filename += ".dat"
-                        output_file = output_path / (filename)
+                        if (filenameOffset != 0):
+                            reader.seek(pointer + filenameOffset)
+                            filename = reader.read_str()
+                            hash = True
+                            output_path = directory / Path(Myfilename + ".unpack") / Path(filename).parent
+                            output_path.mkdir(parents=True, exist_ok=True)
+                        output_file = output_path / (os.path.basename(filename))
                         with open(output_file, "wb") as fe:
                             fe.write(fileData)
                             print("writing to", output_file, offset, size2)
                         unique_id += 1
+                    
                     reader.seek(stay2)
                     reader.read_uint32(1)
                     file = {
-                        "Hash?": hash,
-                        "File Name": filename,
+                        "Has file name?": hash,
+                        "Path": filename,
                     }
                     section.update({p: file})
                     p += 1
@@ -98,6 +106,7 @@ for dat_file in files:
         with open(output_file, "w") as filejson:
             filejson.write(json.dumps(header, ensure_ascii=False, indent=2))
     elif (Mypath.is_dir()):
+        size4 = 0
         w = BinaryReader()
         size2 = 0
         with open((dat_file + "/manifest.json"), "r") as f:
@@ -122,14 +131,20 @@ for dat_file in files:
             hashes = list()
             for j in range(len(p[f"Section {i}"])-1):
                 listOfFilesInFolder.append(
-                    dat_file + "\\" + p[f"Section {i}"][str(j)]["File Name"])
-                hashes.append(p[f"Section {i}"][str(j)]["Hash?"])
+                    dat_file + "\\" + p[f"Section {i}"][str(j)]["Path"])
+                hashes.append(p[f"Section {i}"][str(j)]["Has file name?"])
             identifier = folderlist[i]
             size = 0
             for elem in listOfFilesInFolder:
                 if (elem != (dat_file + "\\" + "BLANK")):
                     size += os.path.getsize(elem)
                     test = 16 - (size % 16)
+                    if (test != 16):
+                        size += test
+            for h in range(len(hashes)):
+                if hashes[h]:
+                    name = listOfFilesInFolder[h].replace(dat_file + "\\","")
+                    test = 16 - (len(name) % 16)
                     if (test != 16):
                         size += test
             size2 += size+((len(listOfFilesInFolder)*16)+16)
@@ -164,19 +179,35 @@ for dat_file in files:
                     w.write_uint32(os.path.getsize(elem))
                 else:
                     w.write_uint32(0)
-                w.write_uint32(int(hashes[k]))
-                k += 1
+                w.write_uint32(0)
                 w.write_uint32(0)
                 if (elem != (dat_file + "\\" + "BLANK")):
                     size3 += os.path.getsize(elem)
+                if (hashes[k]):
+                    name = listOfFilesInFolder[k].replace(dat_file + "\\","")
+                    test = 16 - (len(name) % 16)
+                    size3+=len(name)
                 if ((16 - (size3 % 16)) != 16):
                     size3 += (16 - (size3 % 16))
+                k += 1
+            j = 0
             for elem in listOfFilesInFolder:
                 if (elem != (dat_file + "\\" + "BLANK")):
                     with open(elem, "rb") as file:
                         w.write_bytes(file.read())
+                stringPointer=0
+                if hashes[j]:
+                    name = listOfFilesInFolder[j].replace(dat_file + "\\","")
+                    stringPointer = w.pos()-pointer
+                    print(stringPointer)
+                    stay6 = w.pos()
+                    w.seek(pointer + 24 + (j * 16))
+                    w.write_uint32(stringPointer)
+                    w.seek(stay6)
+                    w.write_str(name, True)
+                j+=1
                 if (w.pos() % 16 != 0):
-                    w.pad(16 - (w.pos() % 16))
+                    w.pad(16 - (w.pos() % 16)) 
             secSize = w.pos()
             w.seek(stay-12)
             w.write_uint32(secSize-pointer)
